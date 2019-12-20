863c084c88fd059d0aee7288ee039c678d626ac9
Niyaz <niyazm524@gmail.com>
2019-12-20 15:46:36 +0300

    packaging into jar enabled

--- src/main/kotlin/app/MyApp.kt
+++ src/main/kotlin/app/MyApp.kt
@@ -3,4 +3,11 @@ package app
 import tornadofx.*
 import view.GameView
 
-class MyApp : App(GameView::class, Styles::class)
+class MyApp : App(GameView::class, Styles::class) {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            launch<MyApp>(args)
+        }
+    }
+}
